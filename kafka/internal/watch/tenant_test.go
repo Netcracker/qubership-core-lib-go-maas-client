b427d59363e2c60290372e8acd2c3d477f8bd38c
Evgenii Stetsenko <i@estetsenko.com>
2025-10-10 15:34:41 +0300

    chore: increase tests coverage

--- kafka/internal/watch/tenant_test.go
+++ kafka/internal/watch/tenant_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/netcracker/qubership-core-lib-go-maas-client/v3/kafka/model"
 	"github.com/netcracker/qubership-core-lib-go-maas-client/v3/watch"
 	go_stomp_websocket "github.com/netcracker/qubership-core-lib-go-stomp-websocket/v3"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,6 +20,22 @@ const (
 	testNamespace = "test-namespace"
 	timeout       = 10 * time.Second
 )
+
+func newMockSubscription() *go_stomp_websocket.Subscription {
+	return &go_stomp_websocket.Subscription{
+		FrameCh: make(chan *go_stomp_websocket.Frame, 10),
+		Id:      "test-sub",
+		Topic:   "/channels/tenants",
+	}
+}
+
+type mockResource struct {
+	Name string
+}
+
+func (m mockResource) GetClassifier() classifier.Keys {
+	return classifier.Keys{}
+}
 
 func Test_mergeTenantsSubscribed(t *testing.T) {
 	assertions := require.New(t)
@@ -375,5 +392,98 @@ func TestWatcherProcessSafe(t *testing.T) {
 	w.process([]fakeResource{{ID: "x"}}, nil)
 	if !called {
 		t.Fatal("callback was not called")
+	}
+}
+
+func TestMergeTenants_Subscribed(t *testing.T) {
+	b := &TenantWatchBroadcaster[mockResource]{}
+	event := &watch.TenantWatchEvent{
+		Type:    watch.SUBSCRIBED,
+		Tenants: []watch.Tenant{makeTenant(watch.StatusActive)},
+	}
+	changed := b.mergeTenants(event)
+	assert.True(t, changed)
+	assert.Len(t, b.currentTenants, 1)
+}
+
+func TestMergeTenants_ModifiedAndDeleted(t *testing.T) {
+	b := &TenantWatchBroadcaster[mockResource]{}
+	b.currentTenants = []watch.Tenant{makeTenant(watch.StatusActive)}
+
+	// Modified to suspended removes tenant
+	event := &watch.TenantWatchEvent{
+		Type:    watch.MODIFIED,
+		Tenants: []watch.Tenant{makeTenant(watch.StatusSuspended)},
+	}
+	assert.True(t, b.mergeTenants(event))
+	assert.Len(t, b.currentTenants, 0)
+
+	// Deleted removes tenant
+	b.currentTenants = []watch.Tenant{makeTenant(watch.StatusActive)}
+	event = &watch.TenantWatchEvent{
+		Type:    watch.DELETED,
+		Tenants: []watch.Tenant{makeTenant(watch.StatusActive)},
+	}
+	assert.True(t, b.mergeTenants(event))
+	assert.Len(t, b.currentTenants, 0)
+}
+
+func TestReadEvents_Success(t *testing.T) {
+	sub := newMockSubscription()
+	b := &TenantWatchBroadcaster[mockResource]{tenants: make(chan []watch.Tenant, 1)}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	onConnectCalled := false
+	onConnect := func() { onConnectCalled = true }
+
+	go func() {
+		event := watch.TenantWatchEvent{
+			Type:    watch.SUBSCRIBED,
+			Tenants: []watch.Tenant{makeTenant(watch.StatusActive)},
+		}
+		data, _ := json.Marshal(event)
+		sub.FrameCh <- &go_stomp_websocket.Frame{Body: string(data)}
+		time.Sleep(10 * time.Millisecond)
+		cancel()
+	}()
+
+	err := b.readEvents(ctx, sub, onConnect)
+	assert.ErrorIs(t, err, context.Canceled)
+	assert.True(t, onConnectCalled)
+}
+
+func TestMergeTenants_AllPaths(t *testing.T) {
+	b := &TenantWatchBroadcaster[mockResource]{}
+
+	// SUBSCRIBED
+	e1 := &watch.TenantWatchEvent{Type: watch.SUBSCRIBED, Tenants: []watch.Tenant{makeTenant(watch.StatusActive)}}
+	assert.True(t, b.mergeTenants(e1))
+	assert.Len(t, b.currentTenants, 1)
+
+	// CREATED (ignored)
+	e2 := &watch.TenantWatchEvent{Type: watch.CREATED}
+	assert.False(t, b.mergeTenants(e2))
+
+	// MODIFIED: add suspended
+	b.currentTenants = []watch.Tenant{makeTenant(watch.StatusActive)}
+	e3 := &watch.TenantWatchEvent{Type: watch.MODIFIED, Tenants: []watch.Tenant{makeTenant(watch.StatusSuspended)}}
+	assert.True(t, b.mergeTenants(e3))
+
+	// DELETED
+	b.currentTenants = []watch.Tenant{makeTenant(watch.StatusActive)}
+	e4 := &watch.TenantWatchEvent{Type: watch.DELETED, Tenants: []watch.Tenant{makeTenant(watch.StatusActive)}}
+	assert.True(t, b.mergeTenants(e4))
+	assert.Empty(t, b.currentTenants)
+
+	// UNKNOWN
+	e5 := &watch.TenantWatchEvent{Type: "foo"}
+	assert.False(t, b.mergeTenants(e5))
+}
+
+func makeTenant(status watch.TenantStatus) watch.Tenant {
+	return watch.Tenant{
+		ExternalId: "tenant1",
+		Status:     status,
 	}
 }
